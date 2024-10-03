53d85ca00a5b01bbc372a9bd3944228f270abb59
shriasokan <asokan@usc.edu>
2024-10-02 21:48:41 -0700

    functional server c version

--- C_version/http_server.c
+++ C_version/http_server.c
@@ -63,7 +63,7 @@ int main(int argc, char **argv) {
         printf("Server started.\n");
         printf("\tWaiting for client to connect...\n");
         struct sockaddr_in client_addr;
-        int client_addr_len = sizeof(client_addr);
+        socklen_t client_addr_len = sizeof(client_addr);
         int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 
         if (client_fd == -1) {
@@ -72,5 +72,151 @@ int main(int argc, char **argv) {
         }
 
         printf("Client connected\n");
+
+        if (!fork()) {
+            close(server_fd);
+            handle_connection(client_fd);
+            close(client_fd);
+            exit(0);
+        }
+        close(client_fd);
+    }
+
+    return 0;
+}
+
+void handle_connection(int client_fd) {
+    printf("\n");
+
+    char readBuffer[1024];
+    int bytesReceived = recv(client_fd, readBuffer, sizeof(readBuffer), 0);
+
+    if (bytesReceived == -1) {
+        printf("Receiving failed: %s \n", strerror(errno));
+        exit(1);
+    }
+
+    char *method = strdup(readBuffer);
+    char *content = strdup(readBuffer);
+    printf("Content: %s\n", method);
+    char *reqPath = strtok(readBuffer, " ");
+    reqPath = strtok(NULL, " ");
+
+    int bytesSent;
+
+    if (strcmp(reqPath, "/") == 0) {
+        char *res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+                    "<html><body><h1>C HTTP Server</h1></body></html>";
+        printf("Sending reponse: %s/n", res);
+        bytesSent = send(client_fd, res, strlen(res), 0);
+    }
+    else if (strncmp(reqPath, "/echo/", 6) == 0) {
+        reqPath = strtok(reqPath, "/");
+        reqPath = strtok(NULL, "");
+        int contentLength = strlen(reqPath);
+
+        char response[512];
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, reqPath);
+        printf("Sending response: %s\n", response);
+        bytesSent = send(client_fd, response, strlen(response), 0);
+    }
+    else if (strcmp(reqPath, "/user-agent") == 0) {
+        reqPath = strtok(NULL, "\r\n");
+        reqPath = strtok(NULL, "\r\n");
+        reqPath = strtok(NULL, "\r\n");
+
+        char *body = strtok(reqPath, " ");
+        body = strtok(NULL, " ");
+        int contentLength = strlen(body);
+
+        char response[512];
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, body);
+        printf("Sending response: %s\n", response);
+        bytesSent = send(client_fd, response, strlen(response), 0);
+    }
+    else if (strncmp(reqPath, "/files/", 7) == 0 && strcmp(method, "GET") == 0) {
+        char *filename = strtok(NULL, "");
+        filename = strtok(NULL, "");
+
+        FILE *fp = fopen(filename, "rb");
+        if (!fp) {
+            printf("File not found");
+            char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
+            bytesSent = send(client_fd, res, strlen(res), 0);
+        }
+        else {
+            printf("Opening file %s\n", filename);
+        }
+
+        if (fseek(fp, 0, SEEK_END) < 0) {
+            printf("Error reading the document\n");
+        }
+
+        size_t data_size = ftell(fp);
+        rewind(fp);
+        void *data = malloc(data_size);
+
+        if (fread(data, 1, data_size, fp) != data_size) {
+            printf("Error reading the document\n");
+        }
+
+        fclose(fp);
+        char response[1024];
+        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n%s", data_size, (char *)data);
+        printf("Sending response: %s\n", response);
+        bytesSent = send(client_fd, response, strlen(response), 0);
+    }
+    else if (strncmp(reqPath, "/files/", 7) == 0 && strcmp(method, "POST") == 0) {
+        method = strtok(NULL, "\r\n");
+        method = strtok(NULL, "\r\n");
+        method = strtok(NULL, "\r\n");
+        method = strtok(NULL, "\r\n");
+        method = strtok(NULL, "\r\n");
+        
+        char *contentLengthStr = strtok(method, " ");
+        contentLengthStr = strtok(NULL, " ");
+        int contentLength = atoi(contentLengthStr);
+        char *filename = strtok(reqPath, "/");
+        filename = strtok(NULL, "");
+
+        content = strtok(content, "\r\n");
+        content = strtok(NULL, "\r\n");
+        content = strtok(NULL, "\r\n");
+        content = strtok(NULL, "\r\n");
+        content = strtok(NULL, "\r\n");
+        content = strtok(NULL, "\r\n");
+        content = strtok(NULL, "\r\n");
+
+        printf("\n---\nCreate a file %s with content length: %d\n\n %s\n---\n", filename, contentLength, content);
+        FILE *fp = fopen(filename, "wb");
+        if (!fp) {
+            printf("File could not be opened");
+            char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
+            bytesSent = send(client_fd, res, strlen(res), 0);
+        }
+        else {
+            printf("Opening file %s\n", filename);
+        }
+
+        if (fwrite(content, 1, contentLength, fp) != contentLength) {
+            printf("Error writing the data");
+        }
+
+        fclose(fp);
+        char response[1024];
+        sprintf(response, "HTTP/1.1 201 Created\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content);
+        printf("Sending response: %s\n", response);
+        bytesSent = send(client_fd, response, strlen(response), 0);
+    }
+    else {
+        char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
+        bytesSent = send(client_fd, res, strlen(res), 0);
+    }
+    if (bytesSent < 0) {
+        printf("Send failed\n");
+        exit(1);
+    }
+    else {
+        return;
     }
 }
