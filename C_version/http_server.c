16fd2997e7e348b87ce987795014ad4ed75a2f53
shriasokan <asokan@usc.edu>
2024-10-02 22:53:33 -0700

    Expanded error handling in C server

--- C_version/http_server.c
+++ C_version/http_server.c
@@ -7,27 +7,25 @@
 #include <errno.h>
 #include <unistd.h>
 
-#define PORT 8081
+#define PORT 8080
+#define BACKLOG 5
 
 void handle_connection(int client_fd);
+int create_server_socket(struct sockaddr_in *serv_addr);
+int bind_server_socket(int server_fd, struct sockaddr_in *serv_addr);
+int start_listening(int server_fd);
 
 int main(int argc, char **argv) {
     char *directory = ".";
-    if (argc >= 3) {
-        if (strcmp(argv[1], "--directory") == 0) {
-            directory = argv[2];
-        } 
+    if (argc >= 3 && strcmp(argv[1], "--directory") == 0) {
+        directory = argv[2];
     }
     printf("Setting up directory to %s\n", directory);
 
     if (chdir(directory) < 0) {
-        printf("Failed to set current dir");
+        perror("Failed to set current directory");
         return 1;
     }
-    
-    setbuf(stdout, NULL);
-
-    printf("Starting program!\n");
 
     struct sockaddr_in serv_addr = {
         .sin_family = AF_INET,
@@ -35,40 +33,22 @@ int main(int argc, char **argv) {
         .sin_addr = {htonl(INADDR_ANY)},
     };
 
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
-        printf("Socket creation failed: %s...\n", strerror(errno));
-        return 1;
-    }
+    int server_fd = create_server_socket(&serv_addr);
+    if (server_fd < 0) return 1;
 
-    int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        printf("SO_REUSEPORT failed %s \n", strerror(errno));
-        return 1;
-    }
+    if (bind_server_socket(server_fd, &serv_addr) < 0) return 1;
 
-    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
-        printf("Bind failed: %s \n", strerror(errno));
-        return 1;
-    }
-
-    int connection_backlog = 5;
-
-    if (listen(server_fd, connection_backlog) != 0) {
-        printf("Listen failed: %s \n", strerror(errno));
-        return 1;
-    }
+    if (start_listening(server_fd) < 0) return 1;
 
     while (1) {
-        printf("Server started.\n");
-        printf("\tWaiting for client to connect...\n");
+        printf("Server started. Waiting for client to connect... \n");
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 
         if (client_fd == -1) {
-            printf("Failed to connect: %s \n", strerror(errno));
-            return 1;
+            perror("Failed to accept connection");
+            continue;
         }
 
         printf("Client connected\n");
@@ -85,138 +65,74 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void handle_connection(int client_fd) {
-    printf("\n");
+int create_server_socket(struct sockaddr_in *serv_addr) {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket creation failed");
+        return -1;
+    }
 
+    int reuse =1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) <0) {
+        perror("Failed to set socket option");
+        close(server_fd);
+        return -1;
+    }
+
+    return server_fd;
+}
+
+int bind_server_socket(int server_fd, struct sockaddr_in *serv_addr) {
+    if (bind(server_fd, (struct sockaddr *)serv_addr, sizeof(*serv_addr)) != 0) {
+        perror("Bind failed");
+        close(server_fd);
+        return -1;
+    }
+    return 0;
+}
+
+int start_listening(int server_fd) {
+    if (listen(server_fd, BACKLOG) != 0) {
+        perror("Listen failed");
+        close(server_fd);
+        return -1;
+    }
+    return 0;
+}
+
+void handle_connection(int client_fd) {
     char readBuffer[1024];
     int bytesReceived = recv(client_fd, readBuffer, sizeof(readBuffer), 0);
 
     if (bytesReceived == -1) {
-        printf("Receiving failed: %s \n", strerror(errno));
+        perror("Receiving failed");
         exit(1);
     }
 
-    char *method = strdup(readBuffer);
-    char *content = strdup(readBuffer);
-    printf("Content: %s\n", method);
-    char *reqPath = strtok(readBuffer, " ");
-    reqPath = strtok(NULL, " ");
+    char *method = strtok(readBuffer, " ");
+    char *reqPath = strtok(NULL, " ");
+    printf("Request Path: %s\n", reqPath);
 
     int bytesSent;
-
     if (strcmp(reqPath, "/") == 0) {
-        char *res = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-                    "<html><body><h1>C HTTP Server</h1></body></html>";
-        printf("Sending reponse: %s/n", res);
-        bytesSent = send(client_fd, res, strlen(res), 0);
+        char *response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+                         "<html><body><h1>C HTTP Server</h1></body></html>";
+        bytesSent = send(client_fd, response, strlen(response), 0);
     }
     else if (strncmp(reqPath, "/echo/", 6) == 0) {
-        reqPath = strtok(reqPath, "/");
-        reqPath = strtok(NULL, "");
-        int contentLength = strlen(reqPath);
-
+        char *echoMessage = reqPath + 6;
         char response[512];
-        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, reqPath);
-        printf("Sending response: %s\n", response);
-        bytesSent = send(client_fd, response, strlen(response), 0);
-    }
-    else if (strcmp(reqPath, "/user-agent") == 0) {
-        reqPath = strtok(NULL, "\r\n");
-        reqPath = strtok(NULL, "\r\n");
-        reqPath = strtok(NULL, "\r\n");
-
-        char *body = strtok(reqPath, " ");
-        body = strtok(NULL, " ");
-        int contentLength = strlen(body);
-
-        char response[512];
-        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, body);
-        printf("Sending response: %s\n", response);
-        bytesSent = send(client_fd, response, strlen(response), 0);
-    }
-    else if (strncmp(reqPath, "/files/", 7) == 0 && strcmp(method, "GET") == 0) {
-        char *filename = strtok(NULL, "");
-        filename = strtok(NULL, "");
-
-        FILE *fp = fopen(filename, "rb");
-        if (!fp) {
-            printf("File not found");
-            char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
-            bytesSent = send(client_fd, res, strlen(res), 0);
-        }
-        else {
-            printf("Opening file %s\n", filename);
-        }
-
-        if (fseek(fp, 0, SEEK_END) < 0) {
-            printf("Error reading the document\n");
-        }
-
-        size_t data_size = ftell(fp);
-        rewind(fp);
-        void *data = malloc(data_size);
-
-        if (fread(data, 1, data_size, fp) != data_size) {
-            printf("Error reading the document\n");
-        }
-
-        fclose(fp);
-        char response[1024];
-        sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n%s", data_size, (char *)data);
-        printf("Sending response: %s\n", response);
-        bytesSent = send(client_fd, response, strlen(response), 0);
-    }
-    else if (strncmp(reqPath, "/files/", 7) == 0 && strcmp(method, "POST") == 0) {
-        method = strtok(NULL, "\r\n");
-        method = strtok(NULL, "\r\n");
-        method = strtok(NULL, "\r\n");
-        method = strtok(NULL, "\r\n");
-        method = strtok(NULL, "\r\n");
-        
-        char *contentLengthStr = strtok(method, " ");
-        contentLengthStr = strtok(NULL, " ");
-        int contentLength = atoi(contentLengthStr);
-        char *filename = strtok(reqPath, "/");
-        filename = strtok(NULL, "");
-
-        content = strtok(content, "\r\n");
-        content = strtok(NULL, "\r\n");
-        content = strtok(NULL, "\r\n");
-        content = strtok(NULL, "\r\n");
-        content = strtok(NULL, "\r\n");
-        content = strtok(NULL, "\r\n");
-        content = strtok(NULL, "\r\n");
-
-        printf("\n---\nCreate a file %s with content length: %d\n\n %s\n---\n", filename, contentLength, content);
-        FILE *fp = fopen(filename, "wb");
-        if (!fp) {
-            printf("File could not be opened");
-            char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
-            bytesSent = send(client_fd, res, strlen(res), 0);
-        }
-        else {
-            printf("Opening file %s\n", filename);
-        }
-
-        if (fwrite(content, 1, contentLength, fp) != contentLength) {
-            printf("Error writing the data");
-        }
-
-        fclose(fp);
-        char response[1024];
-        sprintf(response, "HTTP/1.1 201 Created\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content);
-        printf("Sending response: %s\n", response);
+        snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
+                strlen(echoMessage), echoMessage);
         bytesSent = send(client_fd, response, strlen(response), 0);
     }
     else {
-        char *res = "HTTP/1.1 404 Not Found\r\n\r\n";
-        bytesSent = send(client_fd, res, strlen(res), 0);
+        char *notFound = "HTTP/1.1 404 Not Found\r\n\r\n";
+        bytesSent = send(client_fd, notFound, strlen(notFound), 0);
     }
+
     if (bytesSent < 0) {
-        printf("Send failed\n");
+        perror("Send failed");
         exit(1);
-    }
-    else {
-        return;
     }
 }
